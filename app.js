ee7a758d4be19759bbb22ac8e8c359471a02ed56
Camilo Parra <juanparracsfn@gmail.com>
2026-01-02 01:09:35 -0500

    Implement book library and grid UI
    Add myLibrary array and Book id generation using crypto.randomUUID().
    Add functions to add books, render card elements into the book grid

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const form = document.getElementById("newBookForm");
 const showFormBtn = document.getElementById("showFormBtn");
 const closeFormBtn = document.getElementById("closeFormBtn");
+const bookGrid = document.getElementById("bookGrid");
+const submitBtn = document.querySelector(".addBook");
 
 // Open the Form
 showFormBtn.addEventListener("click", () => {
@@ -18,31 +20,55 @@ window.addEventListener("click", (e) => {
   }
 });
 
+// Where the Library thingy happens
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-Book.prototype.info = function () {
-  const readStatus = this.read ? "read" : "not read yet";
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-};
+function addBookToLibrary() {
+  const title = document.getElementById("bookTitle").value;
+  const author = document.getElementById("bookAuthor").value;
+  const pages = document.getElementById("bookPages").value;
+  const read = document.getElementById("alreadyRead").checked;
 
-// // === MODERN WAY ES6+ ===
-// class  {
-//   constructor(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-//   }
-//   infoM() {
-//     const readStatus = this.read ? "read" : "not read yet";
-//     return `CLASSES ${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-//   }
-// }
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  createGrid();
+  closeForm();
+}
+
+function createGrid() {
+  bookGrid.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("card");
+
+    bookCard.innerHTML = `
+    <h3 class="card_title">${book.title}</h3>
+    <p class="card__content"><strong>Author:</strong> ${book.author}</p>
+    <p class="card__content"><strong>Pages:</strong> ${book.pages}</p>
+    <button class="status-btn">${book.read ? "Read" : "Not Read"}</button>`;
+
+    bookGrid.appendChild(bookCard);
+  });
+}
+
+function closeForm() {
+  form.classList.remove("active");
+}
+
+// I am Messy, the add book button
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+});
