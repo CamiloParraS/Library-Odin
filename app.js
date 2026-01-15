40e4058b467d09f692d97e191b91391ab904d505
Camilo Parra <juanparracsfn@gmail.com>
2026-01-15 16:55:58 -0500

    refactored the contructor to use class

--- app.js
+++ app.js
@@ -12,20 +12,34 @@ const submitBtn = document.querySelector(".addBook");
 
 // ====== Contructor ======
 
-function Book(title, author, pages, read) {
-  if (!new.target) {
-    throw Error("You must use the 'new' operator to call the constructor");
-  }
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+// function Book(title, author, pages, read) {
+//   if (!new.target) {
+//     throw Error("You must use the 'new' operator to call the constructor");
+//   }
+//   this.id = crypto.randomUUID();
+//   this.title = title;
+//   this.author = author;
+//   this.pages = pages;
+//   this.read = read;
+// }
 
-Book.prototype.toggleRead = function () {
-  this.read = !this.read;
-};
+// Book.prototype.toggleRead = function () {
+//   this.read = !this.read;
+// };
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  toggleRead() {
+    this.read = !this.read;
+  }
+}
 
 // ====== Logic Funtions ======
 
